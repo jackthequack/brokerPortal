10b49054240c62a3043d1843d766ccaea5ed416c
Akiva Rockland <ar5858@nyu.edu>
2020-11-11 18:10:33 -0500

    passport added

--- src/app.js
+++ src/app.js
@@ -1,18 +1,52 @@
 const express = require('express');
-const db = require('./db.js');
 const mongoose = require('mongoose');
-const Broker = mongoose.model('Broker');
-const Realtor = mongoose.model('Realtor');
-//const passportLocalMongoose = require('passport-local-mongoose');
-//const connectEnsureLogin = require('connect-ensure-login');
-const username = "Quack"
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const LocalStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
 const app = express();
+const db = require('./db.js');
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+const Realtor = mongoose.model('Realtor');
+const Broker = mongoose.model('Broker');
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+const session = require('express-session');
+const sessionOptions = {
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+};
+
+app.use(bodyParser.json());
+app.use(session(sessionOptions));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(Realtor.authenticate()));
+passport.serializeUser(Realtor.serializeUser());
+passport.deserializeUser(Realtor.deserializeUser());
+
+passport.use(new LocalStrategy(Broker.authenticate()));
+passport.serializeUser(Broker.serializeUser());
+passport.deserializeUser(Broker.deserializeUser());
+
+
 const logger = (req, res, next) => {
     console.log(req.method)
     console.log(req.path)
     console.log(req.query)
     next();
 }
+
 
 const formCleaner = (req, res, next) => {
     for(const i in req.query){
@@ -25,66 +59,84 @@ const formCleaner = (req, res, next) => {
 
 const use = () => {
     app.use(express.static('public'));
-    app.set('view engine', 'hbs');
-    app.use(express.urlencoded({extended: false}));
+
     app.use(logger)
-    app.use(formCleaner)
+    //app.use(formCleaner)
 }
 use();
-/*
-note: need to add the HTML and CSS to work with this
-and maybe will need to switch some things around interms
-of how we're storing the user and pass in db
 
-const session = require('express-session');
-const sessionOptions = {
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: false
-};
-
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(session(sessionOptions));
-
-const passport = require('passport');
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(Realtor.createStrategy());
-passport.serializeUser(Realtor.serializeUser());
-passport.deserializeUser(Realtor.deserializeUser());
-
-passport.use(Broker.createStrategy());
-passport.serializeUser(Broker.serializeUser());
-passport.deserializeUser(Broker.deserializeUser());
-
-app.post('/', (req, res, next)=>{
-  passport.authenticate('local', (err, user, info)=>{
-    if(err){return next(err);}
-    if(!user){
-
-      return res.redirect('/?info'+info);
-    }
-    req.logIn(user, function(err){
-      if(err){
-        return next(err);
-      }
-      return res.redirect('/dashboard');
-    });
-  })(req, res, next);
+app.get("/", function (req, res) {
+    res.render("home");
 });
-*/
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+app.post("/register", function (req, res) {
+    var username = req.body.username
+    var password = req.body.password
+    var brokerage = req.body.brokerage
+    var accountType = req.body.accountType
+    var name = req.body.name
 
-app.get('/', (req, res) => {
+    if(accountType == 'R'){
+      Realtor.register(new Realtor({ username: username, brokerage: brokerage, name: name}),
+              password, function (err, user) {
+          if (err) {
+              console.log(err);
+              return res.render("register");
+          }
+
+          passport.authenticate("local")(
+              req, res, function () {
+              res.render("listings");
+          });
+      });
+  }
+
+
+    else{
+      Broker.register(new Broker({ username: username, brokerage: brokerage, name: name}),
+              password, function (err, user) {
+          if (err) {
+              console.log(err);
+              return res.render("register");
+          }
+
+          passport.authenticate("local")(
+              req, res, function () {
+              res.render("listings");
+          });
+      });
+    }
+
+
+});
+
+app.get("/login", function (req, res) {
+    res.render("signin");
+});
+
+app.post("/login", passport.authenticate("local", {successRedirect: '/listings', failureRedirect: '/login'}));
+
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/");
+}
+
+
+
+/*
+app.get('/',  connectEnsureLogin.ensureLoggedIn(), (req, res) => {
     res.render('signin');
 });
 
-app.get('/dashboard', /* connectEnsureLogin.ensureLoggedIn(),*/(req, res)=>{
+app.get('/dashboard', connectEnsureLogin.ensureLoggedIn(),(req, res)=>{
     res.render('home');
 });
-app.get('/salespeople',/* connectEnsureLogin.ensureLoggedIn(),*/ (req, res) => {
+
+*/
+app.get('/salespeople', (req, res) => {
         console.log("QUERY: ", req.query)
         Broker.find({}, (err, myBrokers) => {
             let name = myBrokers[0].firstName + " " + myBrokers[0].lastName;
@@ -117,19 +169,19 @@ app.get('/salespeople',/* connectEnsureLogin.ensureLoggedIn(),*/ (req, res) => {
 
 
 });
-app.get('/performance',/* connectEnsureLogin.ensureLoggedIn(),*/ (req,res) => {
+app.get('/performance',  (req,res) => {
     res.render('performance')
 })
-app.get('/listings',/* connectEnsureLogin.ensureLoggedIn(),*/ (req, res) => {
+app.get('/listings',  (req, res) => {
     res.render('listings')
 })
-app.get('/data',/* connectEnsureLogin.ensureLoggedIn(),*/ (req, res) => {
-    res.render('data')
+app.get('/data', (req, res) => {
+    res.render('data');
 });
-app.get('/messages',/* connectEnsureLogin.ensureLoggedIn(),*/ (req, res) => {
+app.get('/messages',  (req, res) => {
     res.render('messages')
 });
-app.post('/salespeople',/* connectEnsureLogin.ensureLoggedIn(),*/ (req, res) => {
+app.post('/salespeople', (req, res) => {
 
         let newRealtor = new Realtor({name: req.body.name, username: req.body.username, password: req.body.password, broker: "John Doe"})
         newRealtor.save((err, myRealtor) => {
@@ -140,6 +192,10 @@ app.post('/salespeople',/* connectEnsureLogin.ensureLoggedIn(),*/ (req, res) => 
     res.redirect('/salespeople')
 
 })
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
 
 /*
 app.get('/user',
