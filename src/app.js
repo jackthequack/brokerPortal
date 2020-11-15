43f38befafdfe17b9834ddc019da61a79e70dad5
Jack Langberg <jacklangberg2121@gmail.com>
2020-11-15 13:24:57 -0500

    Revert to passport

--- src/app.js
+++ src/app.js
@@ -1,35 +1,52 @@
 const express = require('express');
-const db = require('./db.js');
-const path = require('path');
-const fs = require('fs');
-// const JSON = require('json');
-const crypto = require('crypto')
-const csv = require('fast-csv')
-const multer = require('multer');
 const mongoose = require('mongoose');
-const Broker = mongoose.model('Broker');
-const Realtor = mongoose.model('Realtor');
-//const passportLocalMongoose = require('passport-local-mongoose');
-//const connectEnsureLogin = require('connect-ensure-login');
-const storage = multer.diskStorage({ //Used for dynamic naming of images https://www.digitalocean.com/community/tutorials/nodejs-uploading-files-multer-express
-    destination: './public',
-    filename: function (req, file, callback) {
-      crypto.pseudoRandomBytes(16, function(err, raw) {
-        if (err) {return callback(err);}
-      
-        callback(null, raw.toString('hex') + path.extname(file.originalname));
-      });
-    }
-  });
-  const upload = multer({ storage: storage });
-
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const LocalStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
 const app = express();
+const db = require('./db.js');
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+const Realtor = mongoose.model('Realtor');
+const Broker = mongoose.model('Broker');
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+const session = require('express-session');
+const sessionOptions = {
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+};
+
+app.use(bodyParser.json());
+app.use(session(sessionOptions));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(Realtor.authenticate()));
+passport.serializeUser(Realtor.serializeUser());
+passport.deserializeUser(Realtor.deserializeUser());
+
+passport.use(new LocalStrategy(Broker.authenticate()));
+passport.serializeUser(Broker.serializeUser());
+passport.deserializeUser(Broker.deserializeUser());
+
+
 const logger = (req, res, next) => {
     console.log(req.method)
     console.log(req.path)
     console.log(req.query)
     next();
 }
+
 
 const formCleaner = (req, res, next) => {
     for(const i in req.query){
@@ -42,66 +59,84 @@ const formCleaner = (req, res, next) => {
 
 const use = () => {
     app.use(express.static('public'));
-    app.set('view engine', 'hbs');
-    app.use(express.urlencoded({extended: false}));
+
     app.use(logger)
-    app.use(formCleaner)
+    //app.use(formCleaner)
 }
 use();
-/*
-note: need to add the HTML and CSS to work with this
-and maybe will need to switch some things around interms
-of how we're storing the user and pass in db
 
-const session = require('express-session');
-const sessionOptions = {
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: false
-};
-
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(session(sessionOptions));
-
-const passport = require('passport');
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(Realtor.createStrategy());
-passport.serializeUser(Realtor.serializeUser());
-passport.deserializeUser(Realtor.deserializeUser());
-
-passport.use(Broker.createStrategy());
-passport.serializeUser(Broker.serializeUser());
-passport.deserializeUser(Broker.deserializeUser());
-
-app.post('/', (req, res, next)=>{
-  passport.authenticate('local', (err, user, info)=>{
-    if(err){return next(err);}
-    if(!user){
-
-      return res.redirect('/?info'+info);
-    }
-    req.logIn(user, function(err){
-      if(err){
-        return next(err);
-      }
-      return res.redirect('/dashboard');
-    });
-  })(req, res, next);
+app.get("/", function (req, res) {
+    res.render("home");
 });
-*/
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+app.post("/register", function (req, res) {
+    var username = req.body.username
+    var password = req.body.password
+    var brokerage = req.body.brokerage
+    var accountType = req.body.accountType
+    var name = req.body.name
 
-app.get('/', (req, res) => {
+    if(accountType == 'R'){
+      Realtor.register(new Realtor({ username: username, brokerage: brokerage, name: name}),
+              password, function (err, user) {
+          if (err) {
+              console.log(err);
+              return res.render("register");
+          }
+
+          passport.authenticate("local")(
+              req, res, function () {
+              res.render("listings");
+          });
+      });
+  }
+
+
+    else{
+      Broker.register(new Broker({ username: username, brokerage: brokerage, name: name}),
+              password, function (err, user) {
+          if (err) {
+              console.log(err);
+              return res.render("register");
+          }
+
+          passport.authenticate("local")(
+              req, res, function () {
+              res.render("listings");
+          });
+      });
+    }
+
+
+});
+
+app.get("/login", function (req, res) {
+    res.render("signin");
+});
+
+app.post("/login", passport.authenticate("local", {successRedirect: '/listings', failureRedirect: '/login'}));
+
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/");
+}
+
+
+
+/*
+app.get('/',  connectEnsureLogin.ensureLoggedIn(), (req, res) => {
     res.render('signin');
 });
 
-app.get('/dashboard', /* connectEnsureLogin.ensureLoggedIn(),*/(req, res)=>{
+app.get('/dashboard', connectEnsureLogin.ensureLoggedIn(),(req, res)=>{
     res.render('home');
 });
-app.get('/salespeople',/* connectEnsureLogin.ensureLoggedIn(),*/ (req, res) => {
+
+*/
+app.get('/salespeople', (req, res) => {
         console.log("QUERY: ", req.query)
         Broker.find({}, (err, myBrokers) => {
             let name = myBrokers[0].firstName + " " + myBrokers[0].lastName;
@@ -134,67 +169,33 @@ app.get('/salespeople',/* connectEnsureLogin.ensureLoggedIn(),*/ (req, res) => {
 
 
 });
-app.get('/performance',/* connectEnsureLogin.ensureLoggedIn(),*/ (req,res) => {
+app.get('/performance',  (req,res) => {
     res.render('performance')
 })
-app.get('/listings',/* connectEnsureLogin.ensureLoggedIn(),*/ (req, res) => {
+app.get('/listings',  (req, res) => {
     res.render('listings')
 })
-app.get('/data',/* connectEnsureLogin.ensureLoggedIn(),*/ (req, res) => {
-    
-    Realtor.findOne({name: "Loser McLoserFace"}, (err, myRealtor) => {
-        res.render('data', {data: encodeURIComponent(JSON.stringify(myRealtor.data))});
-    })
-    
+app.get('/data', (req, res) => {
+    res.render('data');
 });
-app.post('/data', upload.single('csvData'), (req, res) => {
-    if(req.file.mimetype !== "text/csv"){
-        res.send("Wrong file type");
-        return;
-      }
-    let fileRows = [];
-    csv.parseFile(req.file.path)
-    .on("data", function (data) {
-      fileRows.push(data); // push each row
-    })
-    .on("end", function () {
-      console.log(fileRows) //contains array of arrays. Each inner array represents row of the csv file, with each element of it a column
-      fs.unlinkSync(req.file.path);   // remove temp file
-      //process "fileRows" and respond
-      setRealtorData();
-    })
-    let setRealtorData = () => {
-        let userData = []
-        for(let i = 0; i < fileRows.length; i++){
-        for(j = 0; j < fileRows[i].length; j++){
-            fileRows[i][j] = parseFloat(fileRows[i][j])
-        }
-        
-        userData.push({lat: fileRows[i][0], long: fileRows[i][1]})
-        }
-        console.log(userData);
-        Realtor.updateOne({name: "Loser McLoserFace"}, {$set: {data: userData}}, function(err, resp) {
-            if(err){console.log(err)}
-            else{console.log("Successful: ", resp.result)}
-    })
-    res.redirect('/data')
-    }
-    
-})
-app.get('/messages',/* connectEnsureLogin.ensureLoggedIn(),*/ (req, res) => {
+app.get('/messages',  (req, res) => {
     res.render('messages')
 });
-app.post('/salespeople',/* connectEnsureLogin.ensureLoggedIn(),*/ (req, res) => {
+app.post('/salespeople', (req, res) => {
 
         let newRealtor = new Realtor({name: req.body.name, username: req.body.username, password: req.body.password, broker: "John Doe"})
         newRealtor.save((err, myRealtor) => {
             if(err) {console.log(err)} else{console.log(myRealtor.name, " SUCCESSFULLY ADDED")}
 
         });
-    
+
     res.redirect('/salespeople')
 
 })
+app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+});
 
 /*
 app.get('/user',
--- src/db.js
+++ src/db.js
@@ -1,47 +1,45 @@
 // db.js
 const mongoose = require('mongoose');
-//const passportLocalMongoose = require('passport-local-mongoose');
-const UserSchema = new mongoose.Schema({
-  name: {type: String, required: true},
-  username: {type: String, required: true},
-  password: {type: String, required: true}, //will turn into a hash
-  listings: [{address: String, listprice: String, clientName: String }], //will be added to
-  data: [{lat: Number, long: Number}],
-  reqID: String,
-  broker: {type: String, required: true}
-});
-//UserSchema.plugin(passportLocalMongoose);
+const passportLocalMongoose = require('passport-local-mongoose');
 
-//mongoose.model('Realtor', UserSchema, 'Realtor');
+const UserSchema = new mongoose.Schema({
+  name: {type: String},
+  username: {type: String, required: true},
+  listings: [{address: String, listprice: String, clientName: String }], //will be added to
+  reqID: String,
+  brokerage: {type: String}
+});
+
+UserSchema.plugin(passportLocalMongoose);
 
 mongoose.model('Realtor', UserSchema);
 
 // my schema goes here!
+
 const BrokerSchema = new mongoose.Schema({
-  firstName: {type: String, required: true},
-  lastName: {type: String, required: true},
-  brokerageName: {type: String, required: true},
+  name: {type: String},
+  brokerage: {type: String},
   salespeople: [UserSchema], //note: this subdocument structure doesn't allow to save nested docs individually. Have to save the parent (broker);
   username: {type: String, required: true},
-  password: {type: String, required: true}, //will turn into a hash
   reqID: String     //similar to homework we can use this to manage cookies
 });
 
 // alternative here is to make a new collection per brokerage
 
-//BrokerSchema.plugin(passportLocalMongoose);
+BrokerSchema.plugin(passportLocalMongoose);
 
-//mongoose.model('Broker', BrokerSchema, 'Broker');
+mongoose.model('Broker', BrokerSchema, 'Broker');
 
-mongoose.model('Broker', BrokerSchema); //the broker collection
+//mongoose.model('Broker', BrokerSchema); //the broker collection
 
+/*
 let dbconf;
 if (process.env.NODE_ENV === 'PRODUCTION') {
  // if we're in PRODUCTION mode, then read the configration from a file
  // use blocking file io to do this...
  const fs = require('fs');
  const path = require('path');
- const fn = path.join(__dirname, '../config.json');
+ const fn = path.join(__dirname, 'config.json');
  const data = fs.readFileSync(fn);
 
  // our configuration file will be in json, so parse it and set the
@@ -54,7 +52,7 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
 }
 //console.log(dbconf)
 mongoose.connect(dbconf);
+*/
 
 
-
-// mongoose.connect('mongodb://localhost/portal');
+mongoose.connect('mongodb://localhost/tester');
