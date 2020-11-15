990f3c39d90c5df099e46d3e074a7a26ce2d565d
Jack Langberg <jacklangberg2121@gmail.com>
2020-11-15 13:01:33 -0500

    Revert to 56e05fced

--- src/db.js
+++ src/db.js
@@ -1,46 +1,47 @@
 // db.js
 const mongoose = require('mongoose');
-const passportLocalMongoose = require('passport-local-mongoose');
-
+//const passportLocalMongoose = require('passport-local-mongoose');
 const UserSchema = new mongoose.Schema({
-  name: {type: String},
+  name: {type: String, required: true},
   username: {type: String, required: true},
+  password: {type: String, required: true}, //will turn into a hash
   listings: [{address: String, listprice: String, clientName: String }], //will be added to
   data: [{lat: Number, long: Number}],
   reqID: String,
-  brokerage: {type: String}
+  broker: {type: String, required: true}
 });
+//UserSchema.plugin(passportLocalMongoose);
 
-UserSchema.plugin(passportLocalMongoose);
+//mongoose.model('Realtor', UserSchema, 'Realtor');
 
 mongoose.model('Realtor', UserSchema);
 
 // my schema goes here!
-
 const BrokerSchema = new mongoose.Schema({
-  name: {type: String},
-  brokerage: {type: String},
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  brokerageName: {type: String, required: true},
   salespeople: [UserSchema], //note: this subdocument structure doesn't allow to save nested docs individually. Have to save the parent (broker);
   username: {type: String, required: true},
+  password: {type: String, required: true}, //will turn into a hash
   reqID: String     //similar to homework we can use this to manage cookies
 });
 
 // alternative here is to make a new collection per brokerage
 
-BrokerSchema.plugin(passportLocalMongoose);
+//BrokerSchema.plugin(passportLocalMongoose);
 
-mongoose.model('Broker', BrokerSchema, 'Broker');
+//mongoose.model('Broker', BrokerSchema, 'Broker');
 
-//mongoose.model('Broker', BrokerSchema); //the broker collection
+mongoose.model('Broker', BrokerSchema); //the broker collection
 
-/*
 let dbconf;
 if (process.env.NODE_ENV === 'PRODUCTION') {
  // if we're in PRODUCTION mode, then read the configration from a file
  // use blocking file io to do this...
  const fs = require('fs');
  const path = require('path');
- const fn = path.join(__dirname, 'config.json');
+ const fn = path.join(__dirname, '../config.json');
  const data = fs.readFileSync(fn);
 
  // our configuration file will be in json, so parse it and set the
@@ -53,7 +54,7 @@ if (process.env.NODE_ENV === 'PRODUCTION') {
 }
 //console.log(dbconf)
 mongoose.connect(dbconf);
-*/
 
 
-mongoose.connect('mongodb://localhost/tester');
+
+// mongoose.connect('mongodb://localhost/portal');
