f88720652741eff8aac1f5478b0467d44c7d868b
Akiva Rockland <ar5858@nyu.edu>
2020-11-18 15:29:53 -0500

    final push before milestone3 deployment

---
+++ src/public/js/main.js
@@ -0,0 +1,12 @@
+const socket = io();
+const chatform = document.getElementById('chat-form')
+socket.on('message', message => {
+    
+    console.log(message);
+})
+
+chatform.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const msg = event.target.elements.m.value;
+    socket.emit('chatMessage', msg)
+})
